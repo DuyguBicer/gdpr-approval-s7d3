6d344f2fa283fe73724df0d53d2bd28d5def4c63
DuyguBicer <duygubicer95@gmail.com>
2025-06-15 04:32:27 +0300

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+
+const initialForm = {
+  email: '',
+  password: '',
+  terms: false,
+};
+
+export default function Login() {
+  const [form, setForm] = useState(initialForm);
+  const [signIn, setSignIn] = useState();
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    let { name, value, type, checked } = event.target;
+    value = type === 'checkbox' ? checked : value; //checkbox'ın checked değeri önemli çünkü checkbox'a değer girmediğimizde de bir valuesu var ve hiç değişmiyor. Bir şey seçtiğimizde değişen şey checked değeri.
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
+      .then((res) => {
+        const user = res.data.find(
+          (item) => item.password == form.password && item.email == form.email
+        );
+        if (user) {
+          setForm(initialForm);
+          history.push('/main');
+        } else {
+          history.push('/error');
+        }
+      });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+        <Input
+          id="exampleEmail"
+          name="email"
+          placeholder="Enter your email"
+          type="email"
+          onChange={handleChange}
+          value={form.email}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">Password</Label>
+        <Input
+          id="examplePassword"
+          name="password"
+          placeholder="Enter your password "
+          type="password"
+          onChange={handleChange}
+          value={form.password}
+        />
+      </FormGroup>
+
+      <FormGroup check>
+        <Input type="checkbox" id="terms" name="terms" checked={form.terms} />
+        <Label htmlFor="terms" check>
+          Check me out
+        </Label>
+      </FormGroup>
+
+      <FormGroup className="text-center p-4">
+        <Button color="primary">Sign In</Button>
+      </FormGroup>
+    </Form>
+  );
+}
